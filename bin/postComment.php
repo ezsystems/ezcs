d89728a454a08c5c0a438a16e316a64c5ee46aa5
André R <ar@ez.no>
2013-10-23 10:32:31 +0200

    Add scripts used for pull request code style checking for further improvment
    Feel free to suggest changes to these, main thing we aim for short term is to add css and js handling.
    Doc would also be nice :)

---
+++ bin/postComment.php
@@ -0,0 +1,16 @@
+#!/usr/bin/php
+<?php
+
+$ch = curl_init( "https://api.github.com/repos/ezsystems/ezpublish-kernel/issues/$argv[1]/comments" );
+
+curl_setopt( $ch, CURLOPT_HTTPAUTH, CURLAUTH_BASIC );
+curl_setopt( $ch, CURLOPT_USERPWD, file_get_contents( $argv[3] ) );
+curl_setopt( $ch, CURLOPT_SSLVERSION, 3 );
+curl_setopt( $ch, CURLOPT_HEADER, true );
+curl_setopt( $ch, CURLOPT_POST, true );
+curl_setopt( $ch, CURLOPT_USERAGENT, "ezrobot PR CodeSniffer" );
+curl_setopt( $ch, CURLOPT_HTTPHEADER, array( 'Content-Type: application/json' ) );
+curl_setopt( $ch, CURLOPT_POSTFIELDS, json_encode( array( "body" => "This Pull Request does not respect our [Coding Standards](https://github.com/ezsystems/ezcs), please, see the report below:\n\n```\n" . file_get_contents( $argv[2] ) . "\n```" ) ) );
+
+curl_exec( $ch );
+curl_close( $ch );
---
+++ bin/pull-request-cs.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+phpcs --report-full=logs/report.txt $*
+EXIT_CODE="$?"
+if [ $EXIT_CODE -eq "1" ]; then
+  postComment.php $(grep -l $(git rev-parse HEAD) .git/refs/remotes/origin/pr/*/head | sed 's@\.git/refs/remotes/origin/pr/@@;s@/head@@') $PWD/logs/report.txt $PWD/github_credentials.txt
+fi
