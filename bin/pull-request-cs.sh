ab1668ccab9632e0c3510d41f071f3c58f2c8a83
Damien Pobel <dp@ez.no>
2013-10-28 16:00:58 +0100

    Changed: use the login and password stored in Jenkins' global password list

--- bin/pull-request-cs.sh
+++ bin/pull-request-cs.sh
@@ -2,5 +2,5 @@
 phpcs --report-full=logs/report.txt $*
 EXIT_CODE="$?"
 if [ $EXIT_CODE -eq "1" ]; then
-  postComment.php $(grep -l $(git rev-parse HEAD) .git/refs/remotes/origin/pr/*/head | sed 's@\.git/refs/remotes/origin/pr/@@;s@/head@@') $PWD/logs/report.txt $PWD/github_credentials.txt
+  postComment.php $(grep -l $(git rev-parse HEAD) .git/refs/remotes/origin/pr/*/head | sed 's@\.git/refs/remotes/origin/pr/@@;s@/head@@') $PWD/logs/report.txt
 fi
