9bb6606db59885879c6b8a9be8040059b8f39468
Damien Pobel <dp@ez.no>
2013-10-28 17:32:26 +0100

    Fix EZP-21814: Configure CI to run jshint and csslint on PR
    Added support for jshint

--- bin/pull-request-cs.sh
+++ bin/pull-request-cs.sh
@@ -1,6 +1,37 @@
 #!/bin/sh
-phpcs --report-full=logs/report.txt $*
-EXIT_CODE="$?"
-if [ $EXIT_CODE -eq "1" ]; then
-  postComment.php $(grep -l $(git rev-parse HEAD) .git/refs/remotes/origin/pr/*/head | sed 's@\.git/refs/remotes/origin/pr/@@;s@/head@@') $PWD/logs/report.txt
+# Script to test the coding standard on github pull request
+# The first argument is to the tool to use to check the CS, the
+# following arguments depend on the choosen tool.
+#
+# Arguments for PHP:
+# phpcs -n --extensions=php --ignore='*/_fixtures/*' --standard=ezcs eZ/
+# 
+# Arguments for JavaScript:
+# jshint "<list or directories separated by space to search for .js file>" --verbose
+
+EXIT_CODE=0
+REPORT="logs/report.txt"
+TOOL=$1
+
+shift
+
+if [ "$TOOL" = "phpcs" ] ; then
+    phpcs --report-full="$REPORT" $*
+    EXIT_CODE=$?
+elif [ "$TOOL" = "jshint" ] ; then
+    SRC=$1
+    shift
+    for f in `find $SRC -iname \*.js -print` ; do
+        jshint $* "$f" 2>&1 >> $REPORT
+        LOCAL_EXIT_CODE=$?
+        [ $LOCAL_EXIT_CODE -ne 0 ] && echo "--------" >> $REPORT
+        EXIT_CODE=`expr $EXIT_CODE + $LOCAL_EXIT_CODE`
+    done
 fi
+
+
+if [ $EXIT_CODE -ne 0 ] ; then
+    postComment.php $(grep -l $(git rev-parse HEAD) .git/refs/remotes/origin/pr/*/head | sed 's@\.git/refs/remotes/origin/pr/@@;s@/head@@') "$PWD/$REPORT"
+fi
+
+[ -f "$REPORT" ] && rm "$REPORT"
